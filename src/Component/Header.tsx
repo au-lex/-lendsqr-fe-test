433ebd228296bf332afb693fb2c065858b21318c
au-lex <aulexc4d2@gmail.com>
2024-02-17 09:58:47 +0100

    features

---
+++ src/Component/Header.tsx
@@ -0,0 +1,21 @@
+
+
+
+import React from 'react';
+
+// Define the type alias for the props
+// type Propss = {
+//     name: string;
+//     age: number;
+// };
+
+// Define the functional component
+const Header : React.FC = () => {
+    return (
+        <div>
+            heloo
+        </div>
+    );
+};
+
+export default Header ;
